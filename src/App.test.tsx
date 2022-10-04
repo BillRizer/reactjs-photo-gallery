de43a3b360826e6ad64d9ff94c7416932c176f7c
odin <odin@debian.rz>
2022-10-04 15:47:51 -0300

    chore(env): add jest package and first test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders text in App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/uau this a simple test?/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">uau this a simple test?</div>;
 }
 
 export default App;
