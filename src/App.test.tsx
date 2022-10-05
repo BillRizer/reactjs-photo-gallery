b7e8040fe158261c36fa7a7e76be489a970d1005
odin <odin@debian.rz>
2022-10-05 18:11:22 -0300

    temp(misc): remove trash and config jest

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders text in App component', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/uau this a simple test?/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ src/global/test-imports.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { render, screen,fireEvent } from "@testing-library/react";
+
+export { React, render, screen,fireEvent };
