8aca4fd1b05ba6333bae0c4500a65fba1ce057b6
odin <odin@debian.rz>
2022-10-05 01:02:25 -0300

    feat(theme): multi theme enabled * it has bugs, but works

--- src/global/styles/styled.d.ts
+++ src/global/styles/styled.d.ts
@@ -1,5 +1,5 @@
 import 'styled-components';
-import theme from './theme';
+import theme from './theme-light';
 
 declare module "styled-components" {
     type ThemeType = typeof theme
---
+++ src/global/styles/theme-dark.ts
@@ -0,0 +1,34 @@
+const theme = {
+  colors: {
+    header: "#3E3D42",
+    primary: "#5636D3",
+
+    secondary: "#FF872C",
+    secondary_light: "rgba(255,135,44,0.5)",
+
+    success: "#12A454",
+    success_light: "rgba(18,164,84,0.5)",
+
+    attention: "#E83F5B",
+    attention_light: "rgba(232,63,91,0.5)",
+
+    shape: "#dddddd",
+    shape_light: "#f1f1f1",
+
+    title: "#363F5F",
+    title_inv:"#F0F2F5",
+    text: "#969CB2",
+    background: "#F0F2F5",
+  },
+  fonts: {
+    regular: "Roboto_400Regular",
+    medium: "Roboto_500Medium",
+    bold: "Roboto_700Bold",
+    sm: "12px",
+    md: "16px",
+    lg: "19px",
+    h1: "30px",
+    h2: "24px",
+  },
+};
+export default theme;
