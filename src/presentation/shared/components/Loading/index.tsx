26e6c6ac510c9afbfb23956acf0aa2ccde40e88a
odin <odin@debian.rz>
2022-10-05 00:31:00 -0300

    feat(loading): add component

---
+++ src/presentation/shared/components/Loading/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useCallback, useEffect, useRef } from 'react';
+
+import {
+    Background,
+    Brand,
+} from './styles';
+
+import logo from '../../../../assets/icons/logo.png';
+ 
+interface IProps{
+    isActive:Boolean|undefined
+}
+const LoadingComponent: React.FC<IProps> = ({isActive}) => {
+    return <>
+    {(isActive&&(
+        <Background>
+                <Brand>
+                    <span className="content">
+                        <span className="icon">
+                            <img src={logo} />
+                        </span>
+                        <span className="spin"></span>
+                        <span className="spin2"></span>
+                        <span className="spin3"></span>
+                    </span>
+                    
+                </Brand>
+               
+        </Background>
+    )
+    )}
+    </>
+};
+export default LoadingComponent;
---
+++ src/presentation/shared/components/Loading/styles.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+export const Brand = styled.div`
+.content{
+    z-index: 999;
+    width: 110px;
+    height: 110px;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  }
+  .icon{
+    z-index: 999;
+    width: 84px;
+    height: 84px;
+    border-radius: 50%;
+    padding: 22px;
+    position: relative;
+  }
+  .icon img{
+    width: 84px;
+    border-radius: 100%;
+  }
+  @keyframes spinning {
+    from { transform: rotate(0deg) }
+    to { transform: rotate(360deg) }
+  }
+  .spin2::after{
+    animation-duration: 2s!important;
+    background:orange!important;
+  }
+  .spin3::after{
+    background: purple!important;
+    opacity: 0.2;
+    width: 95px!important;
+    height: 95px!important;
+    display: flex!important;
+    padding: 10px;
+  }
+  .spin::after,.spin2::after,.spin3::after{
+    content: '';
+    z-index: -1;
+    width: 111px;
+    background: #ff9000;
+    height: 111px;
+    top: 0;
+    left: 0;
+    border-radius: 40%;
+    position: absolute;
+    display: block;
+    
+    animation-name: spinning;
+    animation-duration: 5s;
+    animation-iteration-count: infinite;
+    /* linear | ease | ease-in | ease-out | ease-in-out */
+    animation-timing-function: linear;
+  }
+`
+
+export const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  
+
+&::after{
+    content: '';
+    background: #000;
+    opacity: 0.5;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+}
+    
+`;
