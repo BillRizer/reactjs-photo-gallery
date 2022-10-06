9e42c9e84165078257d42f44ab8428042c90a604
odin <odin@debian.rz>
2022-10-06 00:33:16 -0300

    feat(notFoundPage): create notfound page

--- src/presentation/pages/NotFoundPage.tsx
+++ src/presentation/pages/NotFoundPage.tsx
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from "react";
-export const NotFoundPage= ()=> {
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import ImgConfusedLost from "../../assets/img/confused-lost.gif";
+import { DividerStyled } from "../shared/components-styled/divider";
+const NotFoundStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  flex-direction: column;
+`;
+const MaskStyled = styled.div`
+  
+`;
+export const NotFoundPage = () => {
+  const {t} = useTranslation()
   return (
-    <div>
-     NotFoundPage
-    </div>
+    <NotFoundStyled>
+      <DividerStyled size="40px"></DividerStyled>
+      <h1>{t('notfound.notfound')}</h1>
+      <p>{t('notfound.buy_coffe')}</p>
+      <MaskStyled>
+        <img src={ImgConfusedLost} alt="" />
+      </MaskStyled>
+    </NotFoundStyled>
   );
-}
-
+};
