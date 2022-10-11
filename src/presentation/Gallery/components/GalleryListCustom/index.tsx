d7621dae96d715412a1cae14ff5225d93876b7c8
BillRizer <teliz.pablo@gmail.com>
2022-10-11 14:28:49 -0300

    refactor(GalleryListCustom): refactor front for photos boxes

--- src/presentation/Gallery/components/GalleryListCustom/index.tsx
+++ src/presentation/Gallery/components/GalleryListCustom/index.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { IGalleryData } from "../../../../application/types/gallery.type";
-import { Photo } from "./style";
+import { Photo, PhotoBox,PhotoMask, PhotoDesc, Photos } from "./style";
 
 interface Props {
-  data:IGalleryData;
+  data: IGalleryData;
 }
 
-export const GalleryListCustom= ({data}:Props)=> {
-  
+export const GalleryListCustom = ({ data }: Props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        {data.photos && data.photos.length >0 &&
-          <div style={{ display: "flex",flexFlow:"row wrap"}}>
-            {data.photos.map((photo,index) => (
-              <Photo src={photo.src} key={photo.id} />
-            ))}
-          </div>
-          
-        }
-      </header>
-    </div>
+    <>
+      {data.photos && data.photos.length > 0 && (
+        <Photos>
+          {data.photos.map((photo, index) => (
+            <PhotoBox>
+              <PhotoMask>
+                <Photo src={photo.src} key={photo.id} />
+                <PhotoDesc></PhotoDesc>
+              </PhotoMask>
+            </PhotoBox>
+          ))}
+        </Photos>
+      )}
+    </>
   );
-}
-
+};
--- src/presentation/Gallery/components/GalleryListCustom/style.ts
+++ src/presentation/Gallery/components/GalleryListCustom/style.ts
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 
 export const Photo = styled.img`
-max-height: 400px
-`
+  max-height: 200px;
+`;
+export const PhotoBox = styled.div`
+  padding: 10px;
+  background: #f1f1f1;
+  box-shadow: 1px 2px 5px 0px #ccc;
+  margin: 10px;
+  border-radius: 3px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+`;
+export const PhotoMask = styled.div`
+  max-height: 200px;
+  max-width: 300px;
+  overflow: hidden;
+  border-radius: 3px;
+`;
+export const PhotoDesc = styled.div``;
+export const Photos = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+`;
