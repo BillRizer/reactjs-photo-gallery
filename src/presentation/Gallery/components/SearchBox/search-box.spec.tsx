46042b54a7ae028a8e1e8e29cfec95739208f920
BillRizer <teliz.pablo@gmail.com>
2022-10-11 12:40:08 -0300

    test(SearchBox): finish tests

--- src/presentation/Gallery/components/SearchBox/search-box.spec.tsx
+++ src/presentation/Gallery/components/SearchBox/search-box.spec.tsx
@@ -1,52 +1,86 @@
-import {React,fireEvent,render,screen} from '../../../../global/test-imports'
-import {SearchBoxComponent} from './index'
-import { useTranslation } from 'react-i18next';
-import { ThemeProvider } from 'styled-components';
-import theme from '../../../../global/styles/theme-dark';
-import { MockTheme } from '../../../../global/utils/test-wrap-theme';
+import {
+  React,
+  fireEvent,
+  render,
+  screen,
+} from "../../../../global/test-imports";
+import { SearchBoxComponent } from "./index";
+import { MockTheme } from "../../../../global/utils/test-wrap-theme";
 
- 
+const setup = (overrides?: any) => {
+  const props = {
+    setSearchQuery: () => {},
+    searchQuery: "",
+    submit: () => {},
+    ...overrides,
+  };
+  render(
+    <MockTheme>
+      <SearchBoxComponent {...props} />
+    </MockTheme>
+  );
+};
 
+describe("Gallery - SearchBox component", () => {
+  test("should render text search in placeholder", () => {
+    setup();
 
-describe("Gallery - search box component", () => {
-    test('should render text search in placeholder', () => {
-        render(<MockTheme><SearchBoxComponent searchQuery='' setSearchQuery={()=>{}} /></MockTheme>);
-        const inputNode = screen.getByPlaceholderText(/search/i);
-        expect(inputNode).toHaveAttribute("placeholder", "search")
-    });
+    const inputNode = screen.getByPlaceholderText(/search/i);
+    expect(inputNode).toHaveAttribute("placeholder", "search");
+  });
+
+  test("should render icon image search", () => {
+    setup();
+
+    const imageElement = screen.getByRole("img");
+    expect(imageElement).toBeInTheDocument();
+  });
+
+  test("should disable button when input is empty", () => {
+    setup();
+
+    const buttonNode = screen.getByRole("button");
+    expect(buttonNode).toHaveAttribute("disabled", "");
+  });
+
+  test("should not disable button when input has text", () => {
+    setup({ searchQuery: "abc" });
+
+    const buttonNode = screen.getByRole("button");
+
+    expect(buttonNode).not.toHaveAttribute("disabled", "");
+  });
+
+  test("should call change func when typing", () => {
+    const mock = jest.fn();
+    setup({ setSearchQuery: mock });
+    const inputNode = screen.getByTestId("input");
+
+    fireEvent.change(inputNode, { target: { value: "1" } });
+    fireEvent.change(inputNode, { target: { value: "12" } });
+
+    expect(mock).toBeCalledTimes(2);
+  });
+
+  test("should call submit when button clicked", () => {
+    const mock = jest.fn();
+    setup({searchQuery:"Fake Search", submit: mock });
     
-    test('should render icon image search', () => {
-        render(<MockTheme><SearchBoxComponent searchQuery='' setSearchQuery={()=>{}} /></MockTheme>);
-        const imageElement = screen.getByRole('img')
-        expect(imageElement).toBeInTheDocument();
-    });
+    const buttonNode = screen.getByTestId("submit");
+    fireEvent.click(buttonNode)
 
-    test('should disable button when input is empty', () => {
-        render(<MockTheme><SearchBoxComponent searchQuery='' setSearchQuery={()=>{}} /></MockTheme>);
-        const buttonNode = screen.getByRole("button");
-        expect(buttonNode).toHaveAttribute("disabled","")
-    });
+    expect(mock).toBeCalledTimes(1);
+  });
 
-    test('should not disable button when input has text', () => {
-        render(<MockTheme><SearchBoxComponent searchQuery='abc' setSearchQuery={()=>{}} /></MockTheme>);
+  test("should prevent call submit when button disabled and clicked", () => {
+    const mock = jest.fn();
+    setup({ submit: mock });
+    
+    const buttonNode = screen.getByTestId("submit");
+    fireEvent.click(buttonNode)
 
-        const buttonNode = screen.getByRole("button");
-        
-        expect(buttonNode).not.toHaveAttribute("disabled","")
-    });
+    expect(mock).toBeCalledTimes(0);
+  });
 
-    test('should call change func when typing', () => {
-        const props = {
-            setSearchQuery: jest.fn(),
-            searchQuery:''
-        }
-        render(<MockTheme><SearchBoxComponent {...props} /></MockTheme>);
-        const inputNode = screen.getByTestId('input')
-
-        fireEvent.change(inputNode,{target:{value:'1'}})
-        fireEvent.change(inputNode,{target:{value:'12'}})
-        
-        expect(props.setSearchQuery).toBeCalledTimes(2)
-    });
 
 });
