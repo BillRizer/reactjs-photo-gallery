a8253d56c913ae437e474be9faebbea5c74fc062
BillRizer <teliz.pablo@gmail.com>
2022-10-16 15:19:13 -0300

    feat(Pagination): move cursor when click in arrow button

--- src/presentation/shared/components/Pagination/index.tsx
+++ src/presentation/shared/components/Pagination/index.tsx
@@ -11,14 +11,14 @@ interface PaginationProps {
   amount: number;
   perPage: number;
   currentPage: number;
-  handleClickPageNumber: any;
+  setPageNumber: any;
 }
 
 export const PaginationComponent = ({
   amount,
   perPage,
   currentPage,
-  handleClickPageNumber,
+  setPageNumber,
   ...props
 }: PaginationProps) => {
   const [innerCurrentPage, setInnerCurrentPage] = useState(currentPage);
@@ -32,12 +32,30 @@ export const PaginationComponent = ({
     label:''
   })
 
+  const handleSetPageNumber = (pageNumber:number) =>{
+    setPageNumber(pageNumber)
+    setInnerCurrentPage(pageNumber);
+  }
+  const handleNextPage = () => {
+    if(innerCurrentPage<totalPages){
+      const next:number=innerCurrentPage+1
+      setInnerCurrentPage(next)
+      setPageNumber(next)
+    }
+  }
+  const handlePrevPage = () => {
+    if(innerCurrentPage>1){
+      const prev:number=innerCurrentPage-1
+      setInnerCurrentPage(prev)
+      setPageNumber(prev)
+    }
+  }
   return (
     
     <ContainerStyled>
       <>
-        <Button {...PropsDefault} data-testid="first" icon={FiChevronsLeft} disabled={innerCurrentPage===1} ></Button>
-        <Button {...PropsDefault} data-testid="prev"  icon={FiChevronLeft} disabled={innerCurrentPage===1} ></Button>
+        <Button {...PropsDefault} data-testid="first" icon={FiChevronsLeft} disabled={innerCurrentPage===1}  onClick={()=>{setInnerCurrentPage(1)}}></Button>
+        <Button {...PropsDefault} data-testid="prev"  icon={FiChevronLeft} disabled={innerCurrentPage===1}  onClick={handlePrevPage}></Button>
         {[...Array(totalPages)].map((page, index) => {
           const _index = index + 1;
 
@@ -48,15 +66,14 @@ export const PaginationComponent = ({
               data-testid="page-number"
               theme={innerCurrentPage === _index ? "primary" : "default"}
               onClick={() => {
-                handleClickPageNumber(_index);
-                setInnerCurrentPage(_index);
+                handleSetPageNumber(_index);
               }}
             />
           );
         })}
 
-        <Button {...PropsDefault} data-testid="next" icon={FiChevronRight} disabled={innerCurrentPage===totalPages?true:false}></Button>
-        <Button {...PropsDefault} data-testid="last" icon={FiChevronsRight} disabled={innerCurrentPage===totalPages?true:false}></Button>
+        <Button {...PropsDefault} data-testid="next" icon={FiChevronRight} disabled={innerCurrentPage===totalPages} onClick={handleNextPage}/>
+        <Button {...PropsDefault} data-testid="last" icon={FiChevronsRight} disabled={innerCurrentPage===totalPages} onClick={()=>{setInnerCurrentPage(totalPages)}}/>
       </>
     </ContainerStyled>
   );
--- src/presentation/shared/components/Pagination/pagination.spec.tsx
+++ src/presentation/shared/components/Pagination/pagination.spec.tsx
@@ -4,7 +4,7 @@ import { PaginationComponent } from ".";
 describe("pagination Component", () => {
 
   test("Should render when exist less than 1 page", () => {
-    render(MountEnv(<PaginationComponent amount={20} currentPage={3} perPage={5} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={20} currentPage={3} perPage={5} setPageNumber />));
 
     const pageNumber = screen.getAllByTestId("page-number");
 
@@ -12,7 +12,7 @@ describe("pagination Component", () => {
   });
 
   test("Should render more one page when the remainder of the division is less than 0.5 (float)", () => {
-    render(MountEnv(<PaginationComponent amount={20} currentPage={2} perPage={19} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={20} currentPage={2} perPage={19} setPageNumber />));
 
     const pageNumber = screen.getAllByTestId("page-number");
 
@@ -20,7 +20,7 @@ describe("pagination Component", () => {
   });
 
   test("Should render more one page when the remainder of the division is greater than 0.5 (float)", () => {
-    render(MountEnv(<PaginationComponent amount={20} currentPage={2} perPage={11} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={20} currentPage={2} perPage={11} setPageNumber />));
 
     const pageNumber = screen.getAllByTestId("page-number");
     
@@ -28,7 +28,7 @@ describe("pagination Component", () => {
   });
   
   test('Should set active for current page', () => {
-    render(MountEnv(<PaginationComponent amount={10} currentPage={2} perPage={3} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={10} currentPage={2} perPage={3} setPageNumber />));
 
     const currentPage = screen.getByText("2")
 
@@ -36,7 +36,7 @@ describe("pagination Component", () => {
 
   });
   test('Should set active for current page out of page numbers', () => {
-    render(MountEnv(<PaginationComponent amount={10} currentPage={3} perPage={5} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={10} currentPage={3} perPage={5} setPageNumber />));
 
     const currentPage = screen.queryByText("3")
 
@@ -45,7 +45,7 @@ describe("pagination Component", () => {
   });
 
   test("Should enable 'last arrow' and 'next arrow' when page number is not max", () => {
-    render(MountEnv(<PaginationComponent amount={10} currentPage={1} perPage={5} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={10} currentPage={1} perPage={5} setPageNumber />));
 
     const lastArrow  = screen.getByTestId('last')
     const nextArrow  = screen.getByTestId('next')
@@ -55,7 +55,7 @@ describe("pagination Component", () => {
   })
 
   test("Should disabled 'last arrow' and 'next arrow' when page number is max", () => {
-    render(MountEnv(<PaginationComponent amount={10} currentPage={2} perPage={5} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={10} currentPage={2} perPage={5} setPageNumber />));
 
     const lastArrow  = screen.getByTestId('last')
     const nextArrow  = screen.getByTestId('next')
@@ -65,7 +65,7 @@ describe("pagination Component", () => {
   })
 
   test("Should enable 'first arrow' and 'prev arrow' when page number is not min", () => {
-    render(MountEnv(<PaginationComponent amount={10} currentPage={2} perPage={4} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={10} currentPage={2} perPage={4} setPageNumber />));
 
     const firstArrow  = screen.getByTestId('first')
     const prevArrow  = screen.getByTestId('prev')
@@ -75,7 +75,7 @@ describe("pagination Component", () => {
   })
 
   test("Should disabled 'first arrow' and 'prev arrow' when page number is min", () => {
-    render(MountEnv(<PaginationComponent amount={10} currentPage={1} perPage={4} handleClickPageNumber />));
+    render(MountEnv(<PaginationComponent amount={10} currentPage={1} perPage={4} setPageNumber />));
 
     const firstArrow  = screen.getByTestId('first')
     const prevArrow  = screen.getByTestId('prev')
@@ -84,4 +84,27 @@ describe("pagination Component", () => {
     expect(prevArrow).toHaveAttribute('disabled');
   })
   
+  test("Should move to next page when click in next button", () => {
+    const mock = jest.fn()
+    render(MountEnv(<PaginationComponent amount={10} currentPage={1} perPage={4} setPageNumber={mock} />));
+
+    const nextArrow  = screen.getByTestId('next')
+    fireEvent.click(nextArrow)
+    const currentPage = screen.getByText(/2/i)
+
+    expect(currentPage).toHaveClass("primary")
+    expect(mock).toHaveBeenCalledWith(2)
+  })
+  
+  test("Should move to prev page when click in prev button", () => {
+    const mock = jest.fn()
+    render(MountEnv(<PaginationComponent amount={10} currentPage={4} perPage={3} setPageNumber={mock} />));
+
+    const prevArrow  = screen.getByTestId('prev')
+    fireEvent.click(prevArrow)
+    const currentPage = screen.getByText(/3/i)
+
+    expect(currentPage).toHaveClass("primary")
+    expect(mock).toHaveBeenCalledWith(3)
+  })
 });
