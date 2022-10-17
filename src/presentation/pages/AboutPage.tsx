f757f8cd70cb7e6f05b980d91701fd6f57c21fda
BillRizer <teliz.pablo@gmail.com>
2022-10-16 21:15:22 -0300

    feat(Aboutpage): add title in page

--- src/presentation/pages/AboutPage.tsx
+++ src/presentation/pages/AboutPage.tsx
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from "react";
-export const AboutPage= ()=> {
-  return (
-    <div>
-     AboutPage
-    </div>
-  );
-}
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
 
+export const AboutPage = () => {
+  const { t } = useTranslation();
+
+  const AboutPageStyld = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    flex-direction: column;
+  `;
+
+  return (
+    <AboutPageStyld>
+      <h1>{t("aboutpage.title")}</h1>
+    </AboutPageStyld>
+  );
+};
