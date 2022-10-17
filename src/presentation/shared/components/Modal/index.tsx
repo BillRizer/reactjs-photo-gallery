2818684a49f92465a14aae769888cb5e03d0e8bb
BillRizer <teliz.pablo@gmail.com>
2022-10-17 20:05:52 -0300

    feat(Modal): add component for modal hook

--- src/presentation/shared/components/Modal/index.tsx
+++ src/presentation/shared/components/Modal/index.tsx
@@ -1,29 +1,82 @@
-import React from 'react';
+import React from "react";
 
 import {
-    Background,
-    Modal,
-    Title,
-} from './styles';
+  Actions,
+  Background,
+  Content,
+  Footer,
+  Header,
+  Modal,
+  Screen,
+  Title,
+  Close,
+} from "./styles";
 
-import { useTranslation } from 'react-i18next';
-import { Button } from '../Button';
- 
-interface IProps{
-    isActive:Boolean|undefined
+import { useTranslation } from "react-i18next";
+import { Button } from "../Button";
+import { IFooterProps, IPayloadProps } from "../../types/modal-payload";
+import { useModal } from "../../hooks/modal";
+import { FcCloseUpMode } from "react-icons/fc";
+import { FiX, FiXCircle } from "react-icons/fi";
+import { IconComponent } from "../Icon";
+
+interface IProps {
+  isActive: Boolean | undefined;
+  payload: IPayloadProps | undefined;
 }
-const ModalComponent: React.FC<IProps> = ({isActive}) => {
-    const {t} = useTranslation()
-    return <>
-    {(isActive&&(
-        <Background>
-                <Modal>
-                    <Title>My title</Title>
-                    static text
-                </Modal>
-        </Background>
-    )
-    )}
-    </>
+const ModalComponent: React.FC<IProps> = ({ isActive, payload }) => {
+  const { t } = useTranslation();
+  const { hideModal } = useModal();
+
+  return isActive ? (
+    <Screen>
+      <Modal>
+        <Close
+          onClick={() => {
+            hideModal();
+          }}
+        >
+          <IconComponent Icon={FiX} size="medium" />
+        </Close>
+
+        {payload?.header && (
+          <Header>
+            <div className="force-big-icon">
+            <IconComponent Icon={payload.header.icon} size="large" />
+            </div>
+            <div>
+            <h2>{payload.header.title}</h2>
+            <p>{payload.header.description}</p>
+
+            </div>
+          </Header>
+        )}
+        <Content>{payload?.body}</Content>
+        <Footer>
+          {payload?.actionsFooter && payload.actionsFooter.length > 0 && (
+            <>
+              {payload?.actionsFooter.map((element: IFooterProps) => (
+                <>
+                  {element.type == "button" && (
+                    <Button
+                      label={element.label || ""}
+                      onClick={element.callBackOnClick}
+                    ></Button>
+                  )}
+                </>
+              ))}
+            </>
+          )}
+        </Footer>
+      </Modal>
+      <Background
+        onClick={() => {
+          hideModal();
+        }}
+      ></Background>
+    </Screen>
+  ) : (
+    <></>
+  );
 };
 export default ModalComponent;
--- src/presentation/shared/components/Modal/styles.ts
+++ src/presentation/shared/components/Modal/styles.ts
@@ -1,30 +1,65 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Modal = styled.div`
-background: ${(props) => props.theme.components.modal.bg};
-color: ${(props) => props.theme.components.modal.primary};
-width: 300px;
-height: 300px;
-z-index: 2;
-`
+  background: ${(props) => props.theme.components.modal.bg};
+  color: ${(props) => props.theme.components.modal.primary};
+  z-index: 99;
+  padding: 60px;
+  border-radius: 4px;
+  position: relative;
+  max-width: 400px;
+`;
+export const Close = styled.div`
+  position: absolute;
+  right: 5px;
+  top: 5px;
+`;
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  margin-bottom: 10px;
+
+  padding-bottom: 17px;
+  & .force-big-icon div{
+    width: 60px;
+    height: 60px;
+    font-size: 60px;
+  }
+  & >div:first-child{
+    margin-right: 20px;
+  }
+`;
 export const Title = styled.div`
-`
-export const Background = styled.div`
+  text-overflow: ellipsis;
+  font-size: 18px;
+  width: 100%;
+  word-break: break-all;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+export const Actions = styled.div``;
+export const Content = styled.div``;
+export const Footer = styled.div``;
+
+export const Screen = styled.div`
+  width: 100%;
+  height: 100vh;
   position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 0;
+`;
+
+export const Background = styled.div`
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-&::after{
-    content: '';
-    background:${(props) => props.theme.components.loading.bg};
-    opacity: 0.9;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-}
-    
+  background: ${(props) => props.theme.components.modal.scree_bg};
+  opacity: 0.9;
 `;
